07ce78ae32b5cc820397cef54c66150c58504765
wanjala stephen <stephenwanjala145@gmail.com>
2023-03-06 18:49:48 +0300

    rename to base

--- base_app/asgi.py
+++ base_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sakko project.
+ASGI config for base_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sakko.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base_app.settings')
 
 application = get_asgi_application()
