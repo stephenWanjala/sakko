6c1d7f5840ec969fa0da9460634ade12e532cd04
wanjala stephen <stephenwanjala145@gmail.com>
2023-03-07 17:23:56 +0300

    Add Milk Model

--- mFarm/models.py
+++ mFarm/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -8,16 +9,43 @@ class Sacco(models.Model):
     email = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
 
+    # TODO("email & phone validation")
+
     def __str__(self):
         return self.name
 
 
 class Farmer(models.Model):
+    # TODO("farmer to be user)
     name = models.CharField(max_length=50)
     sacco = models.ForeignKey(Sacco, on_delete=models.CASCADE)
     phone = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
     address = models.CharField(max_length=50)
 
+    # TODO("email & phone validation)
     def __str__(self):
         return self.name
+
+
+class MilkStatus(models.Model):
+    fresh = models.BooleanField(default=True)
+    spoilt = models.BooleanField(default=False)
+
+    def __str__(self):
+        if self.fresh:
+            return "Fresh"
+        else:
+            "Spoilt"
+
+
+class Milk(models.Model):
+    status = models.ForeignKey(MilkStatus, on_delete=models.CASCADE)
+    farmer = models.ForeignKey(Farmer, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    # date = models.DateField()
+    # price
+
+
+class MilkCollection(models.Model):
+    dateCollected = models.CASCADE()
