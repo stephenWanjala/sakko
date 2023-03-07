b2500423d431c35038fea42d0835c4bf12618913
AlecMaina <amlonwolf@gmail.com>
2023-03-07 16:38:38 +0300

    register models

--- mFarm/admin.py
+++ mFarm/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sacco
+
+admin.site.register(Sacco)
+
+from .models import Farmer
+
+admin.site.register(Farmer)
