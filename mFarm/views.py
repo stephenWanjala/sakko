653135bb3932fc2e0564d0bc1976d5a11dcc838d
wanjala stephen <stephenwanjala145@gmail.com>
2023-03-09 13:37:38 +0300

    Milk evaluation->Milk collection relation

--- mFarm/views.py
+++ mFarm/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from .models import MilkEvaluation
+
 
 def index(request):
-    context = {}
+    evaluation = MilkEvaluation.objects.all()
+    context = {'milk_evaluation': evaluation}
     return render(request, 'mFarm/home.html', context=context)
