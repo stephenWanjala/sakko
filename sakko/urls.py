3d6b4fc6cf7edf7387ea5abc9d60be03cd10722b
AlecMaina <amlonwolf@gmail.com>
2023-03-14 10:39:03 +0300

    admin.site.site_header

--- sakko/urls.py
+++ sakko/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+admin.site.site_header = 'Sacco Milk Procurement'  #Customize admin panel
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mFarm.urls')),
