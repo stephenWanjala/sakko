0442ca1ea28a693682c2f0218d3deaeea6393ddf
wanjala stephen <stephenwanjala145@gmail.com>
2023-04-06 00:45:08 +0300

    Fix bootstrap breaking with django 4.2

--- sakko/settings.py
+++ sakko/settings.py
@@ -9,15 +9,20 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os.path
 from datetime import timedelta
 from pathlib import Path
-
 
 from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_URL = "/static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'bootstrap')
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -45,8 +50,6 @@ INSTALLED_APPS = [
     "rest_framework",
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
-    
-    'bootstrap5',
 ]
 
 REST_FRAMEWORK = {
