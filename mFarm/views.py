5792610c023d29855b2cb45f7f4f3d9720c6aaf7
wanjala stephen <stephenwanjala145@gmail.com>
2023-04-05 21:12:55 +0300

    footer.html

--- mFarm/views.py
+++ mFarm/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,5 +8,5 @@ from .models import MilkEvaluation
 
 def index(request):
     evaluation = MilkEvaluation.objects.all()
-    context = {'milk_evaluation': evaluation}
+    context = {'milk_evaluation': evaluation, 'currentYear': datetime.now().year}
     return render(request, 'mFarm/home.html', context=context)
